803b9e6e90c58b1d88a5cb8f8cb85bf693b38ef3
hagakureshinai <hagakureshinai@gmail.com>
2025-12-25 06:07:18 +0900

    js変更

--- assets/script.js
+++ assets/script.js
@@ -28,6 +28,17 @@ document.addEventListener('DOMContentLoaded', () => {
         novelText.classList.remove('horizontal');
         toggleBtn.textContent = '横書きに戻す'; // ボタンの文字を変える
         localStorage.setItem('novelMode', 'vertical'); // 設定を記憶
+
+        // 「最初の段落」を目印にする
+        setTimeout(() => {
+            // 小説の中の、最初の <p> タグを探す
+            const firstP = novelText.querySelector('p');
+            
+            if (firstP) {
+                // その要素が見える位置まで自動でスクロールしてもらう
+                firstP.scrollIntoView({ inline: "end" });
+            }
+        }, 50); // 時間も少しだけ余裕を持たせて 10 → 50 に変更
     }
 
     // 横書きにする関数
